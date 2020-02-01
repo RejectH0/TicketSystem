5206a30ca45e332608819c7360034fc1b669b82a
Gregg Sperling <gsperling@my.wctc.edu>
2020-02-01 15:10:59 -0600

    01FEB2020 1510

--- TicketSystem/Program.cs
+++ TicketSystem/Program.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace TicketSystem
 {
-    // Version 0.3a //01FEB2020 1415
+    // Version 0.4a //01FEB2020 1510
     class Ticket
     {
         public int TicketNumber;
@@ -82,13 +83,17 @@ namespace TicketSystem
     }
     class Program
     {
+        public Program()
+        {
+            RunMainMenu();
+
+        }
         public static void Main(string[] args)
         {
-            DisplayHeader();
-            DisplayMenu();
+            new Program();
         }
 
-        public static void DisplayHeader()
+        public void DisplayHeader()
         {
             Console.Clear();
             Console.SetCursorPosition(0, 0);
@@ -113,25 +118,120 @@ namespace TicketSystem
             Console.Write("*");
             Console.SetCursorPosition(0, 5);
         }
-        public static void DisplayMenu()
+        public int PrintMainMenu()
         {
+            string menuName = "Main";
             string[] menuChoices = new string[]
             {
-                "Read File",
-                "Create File",
-                "End Program"
+                "Read Ticket File",
+                "Enter New Ticket",
+                "Exit Program"
             };
 
+            Console.WriteLine(menuName + " Menu");
             for (int i = 0; i < menuChoices.Length; i++)
             {
-                Console.WriteLine((i+1) + ": "+menuChoices[i]);
+                Console.WriteLine((i + 1) + ": " + menuChoices[i]);
             }
 
+            return menuChoices.Count();
+        }
+        private void RunMainMenu()
+        {
+            bool userContinue = true;
+           
+            while (userContinue)
+            {
+                DisplayHeader();
+                int menuMin = 1;
+                int menuMax = PrintMainMenu();                        // Show the Main Menu
 
+                // Since the MainMenu should always contain < 10 items, we're only going to ask the user for one character
+                // of input, and we're going to trap the heck out of it since we should never trust user input.
+
+                Console.Write("[=+> # <+=] Choose ({0}-{1})",menuMin,menuMax);           // menu prompt
+                Console.SetCursorPosition(5, Console.CursorTop); // Move the cursor to the '#' in the prompt
+                StringBuilder sb = new StringBuilder(); // Stringbuilder object that will hold the user's menu choice
+                ConsoleKeyInfo cki;                     // ConsoleKeyInfo object
+                cki = Console.ReadKey();                // Get the input from the user
+                sb.Append(cki.KeyChar);                 // Throw that input into the StringBuilder object so it can be parsed.
+
+                int userChoice = 0;
+                try
+                {
+                    userChoice = Int32.Parse(sb.ToString());
+                } catch
+                {
+                    userChoice = 0;
+                }
+
+                switch (userChoice)
+                {
+                    case 1:
+                        ReadFile();
+                        break;
+                    case 2:
+                        EnterTicket();
+                        break;
+                    case 3:
+                        ExitGracefully();
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        break;
+                }
+
+            }
         }
 
-        public static void ReadFile()
+        public void EnterTicket()
         {
+            DisplayHeader();
+            Console.WriteLine("Ticket Entry");
+            PressEnterToContinue();
+        }
+
+        public void ExitGracefully()
+        {
+            Console.Clear();
+            DisplayHeader();
+            Console.WriteLine("Now exiting this application...");
+            WriteFile();
+            PressEnterToContinue();
+            System.Environment.Exit(0);
+        }
+        public void InvalidMenuChoice()
+        {
+            Console.Clear();
+            DisplayHeader();
+            string invalidChoice = "You have made an invalid selection and therefore must try again.";
+            Console.WriteLine("{0,15}",invalidChoice);
+            PressEnterToContinue();
+        }
+
+        public void PressEnterToContinue()
+        {
+            Console.Write("Press Enter To Continue: ");
+            Console.ReadLine();
+        }
+        
+
+        public void ReadFile()
+        {
+            DisplayHeader();
             string fileData = "tickets.csv";
             if (File.Exists(fileData))
             {
@@ -171,20 +271,21 @@ namespace TicketSystem
                 Console.WriteLine("File does not exist.");
             }
 
+            PressEnterToContinue();
         }
 
-        public static void WriteFile()
+        public void WriteFile()
         {
             string fileData = "tickets.csv";
             StreamWriter sw = new StreamWriter(fileData, true);
-            sw.Write("TicketID, Summary, Status, Priority, Submitter, Assigned, Watching1|Watching2\n");
+            sw.Write("TicketID,Summary,Status,Priority,Submitter,Assigned,Watching1|Watching2\n");
                                         // write the code to write out the file data.
 
             sw.Close();
         }
 
 
-        public static void DoBox(String text, Int32? width, Int32? height, Int32? upperLeft, Int32? lowerRight)
+        public void DoBox(String text, Int32? width, Int32? height, Int32? upperLeft, Int32? lowerRight)
         {
             if (upperLeft < 1 || upperLeft > 70 || upperLeft == null)
             {
