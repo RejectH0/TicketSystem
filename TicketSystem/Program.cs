1180212be3619169fba9118bb72ae819f61b47a2
Gregg Sperling <gsperling@my.wctc.edu>
2020-02-01 20:30:48 -0600

    v1.0 Commit: 01FEB2020 2030

--- TicketSystem/Program.cs
+++ TicketSystem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,59 +7,153 @@ using System.Text;
 
 namespace TicketSystem
 {
-    // Version 0.4a //01FEB2020 1510
-    class Ticket
+    // Version 1.0 //01FEB2020 2030
+    class Ticket : IDisposable
     {
-        public int TicketNumber;
+        private static int idNext = 1;
+        private static int idDestroy = -1;
+        public int TicketNumber { get; private set; }
 
         public String Summary { get; set; }
         public String Status { get; set; }
         public String Priority { get; set; }
         public Person Submitter { get; set; }
         public Person Assigned { get; set; }
-        public List<People> Watching { get; set; }
+        public WatcherList<Person> Watching { get; set; }
 
-        public Ticket(int TicketNumber, String Summary, String Status, String Priority, String Submitter, String Assigned, String Watching)
+        public Ticket(String Summary, String Status, String Priority, String Submitter, String Assigned, String Watching)
         {
-            this.TicketNumber = TicketNumber;
+
+            if (idDestroy == -1)
+            {
+                this.TicketNumber = Ticket.idNext;
+                Ticket.idNext++;
+            }
+            else
+            {
+                this.TicketNumber = Ticket.idDestroy;
+            }
             this.Summary = Summary;
             this.Status = Status;
             this.Priority = Priority;
-            this.Submitter = Submitter;
-            this.Assigned = Assigned;
-            this.Watching = new List<People>();
+
+            if (Person.AllPeople.Any(item => item.Equals(Submitter)))
+            {
+                this.Submitter = Person.AllPeople.Find(item => item.FullName.Equals(Submitter));
+            }
+            else
+            {
+                this.Submitter = new Person(Submitter);
+            }
+
+            if (Person.AllPeople.Any(item => item.FullName.Equals(Assigned)))
+            {
+                this.Assigned = Person.AllPeople.Find(item => item.FullName.Equals(Assigned));
+            } else
+            {
+                this.Assigned = new Person(Assigned); 
+            }
+
+            this.Watching = new WatcherList<Person>();
+
+            var personList = Watching.Split('|');
+            Dump(personList);
+
+            for (int g = 0; g < personList.Length; g++)
+            {
+                if (Person.AllPeople.Any(item => item.FullName.Equals(personList[g])))
+                {
+                    this.Watching.Add(Person.AllPeople.Find(item => item.FullName.Equals(personList[g])));
+                }
+                else
+                {
+                    this.Watching.Add(new Person(personList[g]));
+                }
+            }
+
+        }
+        private static void Dump(object o)
+        {
+            string json = JsonConvert.SerializeObject(o, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+        public class WatcherList<Person> : List<Person>
+        {
+            public override string ToString()
+            {
+                var str = "";
+                int thisCount = this.Count();
+
+                if (thisCount > 0)
+                {
+                    for (int g = 0; g < thisCount; g++)
+                    {
+                        str += this.ElementAt(g) + "|";
+                    }
+                }
+                return str.Substring(0, str.Length - 1);
+            }
         }
 
+        public override string ToString()
+        {
+            var str = TicketNumber + "," + Summary + "," + Status + "," + Priority + "," + Submitter + "," + Assigned;
+            int numWatching = Watching.Count();
+
+            if (numWatching > 0)
+            {
+                str += ",";
+
+                str += Watching.ToString();
+
+            }
+
+            return str;
+        }
+
+        public void Dispose()
+        {
+            Ticket.idDestroy = this.TicketNumber;
+        }
+
+        private void LogError(string eMessage, string location)
+        {
+            Console.WriteLine(eMessage, location);
+        }
     }
 
     class Person : IDisposable
     {
-        static private int idNext = 0;
-        static private int idDestroy = -1;
+        private static int idNext = 1;
+        private static int idDestroy = -1;
+        public static List<Person> AllPeople = new List<Person>();
         public int idNumber { get; private set; }
         public String FullName { get; set; }
-        public String Email { get; set; }
-        public String Phone { get; set; }
+        public String Email { get; private set; }
+        public String Phone { get; private set; }
 
-        public Person(String FullName, String Email, String Phone)
+        public Person(String FullName)
         {
-            if(idDestroy == -1)
+            if (idDestroy == -1)
             {
                 this.idNumber = Person.idNext;
                 Person.idNext++;
-            } else
+            }
+            else
             {
                 this.idNumber = Person.idDestroy;
             }
 
             this.FullName = FullName;
-            this.Email = Email;
-            this.Phone = Phone;
+            this.Email = "null";
+            this.Phone = "null";
+
+            Person.AllPeople.Add(this);
         }
 
         public override string ToString()
         {
-            return base.ToString();
+            return FullName;
         }
 
         public void Dispose()
@@ -66,23 +161,11 @@ namespace TicketSystem
             Person.idDestroy = this.idNumber;
         }
 
-        public static implicit operator Person(string v) => new Person(v, "null", "null");
     }
 
-    class People
-    {
-        public People(string psv)
-        {
-            var vals = psv.Split('|');
-
-            for (int i = 0; i < vals.Length; i++)
-            {
-                Person person = new Person(vals[i], "null", "null");
-            }
-        }
-    }
     class Program
     {
+        public static List<Ticket> tickets = new List<Ticket>();
         public Program()
         {
             RunMainMenu();
@@ -101,7 +184,7 @@ namespace TicketSystem
             {
                 Console.Write("*");
             }
-            
+
             int winWidth = Console.WindowWidth - 1;
             Console.SetCursorPosition(winWidth, 1);
             Console.Write("*");
@@ -125,6 +208,8 @@ namespace TicketSystem
             {
                 "Read Ticket File",
                 "Enter New Ticket",
+                "List All Tickets",
+                "List All People",
                 "Exit Program"
             };
 
@@ -139,7 +224,7 @@ namespace TicketSystem
         private void RunMainMenu()
         {
             bool userContinue = true;
-           
+
             while (userContinue)
             {
                 DisplayHeader();
@@ -149,7 +234,7 @@ namespace TicketSystem
                 // Since the MainMenu should always contain < 10 items, we're only going to ask the user for one character
                 // of input, and we're going to trap the heck out of it since we should never trust user input.
 
-                Console.Write("[=+> # <+=] Choose ({0}-{1})",menuMin,menuMax);           // menu prompt
+                Console.Write("[=+> # <+=] Choose ({0}-{1})", menuMin, menuMax);           // menu prompt
                 Console.SetCursorPosition(5, Console.CursorTop); // Move the cursor to the '#' in the prompt
                 StringBuilder sb = new StringBuilder(); // Stringbuilder object that will hold the user's menu choice
                 ConsoleKeyInfo cki;                     // ConsoleKeyInfo object
@@ -160,7 +245,8 @@ namespace TicketSystem
                 try
                 {
                     userChoice = Int32.Parse(sb.ToString());
-                } catch
+                }
+                catch
                 {
                     userChoice = 0;
                 }
@@ -174,11 +260,13 @@ namespace TicketSystem
                         EnterTicket();
                         break;
                     case 3:
-                        ExitGracefully();
+                        ListAllTickets();
                         break;
                     case 4:
+                        ListAllPeople();
                         break;
                     case 5:
+                        ExitGracefully();
                         break;
                     case 6:
                         break;
@@ -204,6 +292,30 @@ namespace TicketSystem
             PressEnterToContinue();
         }
 
+        public void ListAllTickets()
+        {
+            DisplayHeader();
+
+            Console.Write("{0,-10}{1,-25}{2,-10}{3,-10}{4,-15}{5,-15}{6,25}\n", "TicketID", "Summary", "Status", "Priority", "Submitter", "Assigned", "Watching");
+
+            foreach (var item in tickets)
+            {
+                Console.Write("{0,-10}{1,-25}{2,-10}{3,-10}{4,-15}{5,-15}{6,25}\n", item.TicketNumber, item.Summary, item.Status, item.Priority, item.Submitter, item.Assigned, item.Watching);
+            }
+
+            PressEnterToContinue();
+
+        }
+        public void ListAllPeople()
+        {
+            DisplayHeader();
+            Console.Write("{0,-10}{1,-25}{2,-25}{3,-25}\n", "idNumber", "Full Name", "E-Mail", "Phone");
+            foreach (var item in Person.AllPeople)
+            {
+                Console.Write("{0,-10}{1,-25}{2,-25}{3,-25}\n", item.idNumber, item.FullName, item.Email, item.Phone);
+            }
+            PressEnterToContinue();
+        }
         public void ExitGracefully()
         {
             Console.Clear();
@@ -218,7 +330,7 @@ namespace TicketSystem
             Console.Clear();
             DisplayHeader();
             string invalidChoice = "You have made an invalid selection and therefore must try again.";
-            Console.WriteLine("{0,15}",invalidChoice);
+            Console.WriteLine("{0,15}", invalidChoice);
             PressEnterToContinue();
         }
 
@@ -227,12 +339,29 @@ namespace TicketSystem
             Console.Write("Press Enter To Continue: ");
             Console.ReadLine();
         }
-        
+
 
         public void ReadFile()
         {
             DisplayHeader();
-            string fileData = "tickets.csv";
+            Console.WriteLine("Read File");
+            var pathWithEnv = @"%USERPROFILE%\Documents\GS Ticket System-Tickets.txt";
+            var fileData = Environment.ExpandEnvironmentVariables(pathWithEnv);
+
+            Console.WriteLine("File Location: " + pathWithEnv);
+
+            if (!File.Exists(fileData))
+            {
+                DisplayHeader();
+                Console.WriteLine("The file does not exist, so I will create test data for you.");
+                tickets.Add(new Ticket("This is a bug ticket", "Open", "High", "Drew Kjell", "Jane Doe", "Drew Kjell|John Smith|Bill Jones"));
+                Console.WriteLine("Test Data has been committed to memory.");
+                WriteFile();
+                Console.WriteLine("Test Data has been written out to the file.");
+
+                PressEnterToContinue();
+            }
+
             if (File.Exists(fileData))
             {
                 StreamReader sr = new StreamReader(fileData);
@@ -246,20 +375,18 @@ namespace TicketSystem
                     // 1,This is a bug ticket,Open,High,Drew Kjell,Jane Doe,Drew Kjell|John Smith|Bill Jones
                     if (!arr[0].All(char.IsDigit))
                     {
-                        Console.WriteLine("Found Header Row!");
-                        Console.WriteLine("{0,5}{1,15}{2,15}{3,15}{4,15}{5,15}{6,15}", new object[] { arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]});
+                        // The first field is NOT all digits, therefore this is our Header Row. Let's display it on the console for reasons.
+                        Console.Write("{0,-10}{1,-25}{2,-10}{3,-10}{4,-15}{5,-15}{6,25}\n", new object[] { arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6] });
                     }
                     else
                     {
-                        int importTicketNumber = 65535;
-                        try
-                        {
-                            importTicketNumber = Int32.Parse(arr[0]);
-                        } catch
-                        {
-                            importTicketNumber = 65535;
-                        }
-                        Ticket ticket = new Ticket(importTicketNumber, arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
+                        // we're actually discarding the ticket's number as saved in the file (arr[0]) because the Ticket() 
+                        // constructor takes care of all ticket number creation.
+                        // if user does not read file first and instead enters new tickets first, reading the file could result
+                        // in overwriting or loss of data, so we'll just let the constructor deal with adding the ticket number.
+                        // BUT, we'll display the ticket numbers from the file anyway, because why not?
+                        Console.Write("{0,-10}{1,-25}{2,-10}{3,-10}{4,-15}{5,-15}{6,25}\n", new object[] { arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6] });
+                        tickets.Add(new Ticket(arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]));
                     }
                 }
 
@@ -276,12 +403,28 @@ namespace TicketSystem
 
         public void WriteFile()
         {
-            string fileData = "tickets.csv";
-            StreamWriter sw = new StreamWriter(fileData, true);
-            sw.Write("TicketID,Summary,Status,Priority,Submitter,Assigned,Watching1|Watching2\n");
-                                        // write the code to write out the file data.
+            var pathWithEnv = @"%USERPROFILE%\Documents\GS Ticket System-Tickets.txt";
+            var fileData = Environment.ExpandEnvironmentVariables(pathWithEnv);
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fileData, false)) // bool false = NO append.
+                {
+                    // CSV Header
+                    sw.Write("TicketID,Summary,Status,Priority,Submitter,Assigned,Watching1|Watching2\n");
 
-            sw.Close();
+                    foreach (var ticket in tickets)
+                    {
+                        String str = ticket.ToString();
+                        Console.WriteLine("Output to File:\n{0}\n", str);
+                        sw.Write(str + "\n");
+                    }
+                    sw.Close();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.Write("The file could not be written: {0}", e.Message);
+            }
         }
 
 
